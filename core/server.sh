948cf9e59786edd4fb6d17fb436f81969efbdb3a
Evil Devil <66058079+zahrairshad@users.noreply.github.com>
2020-06-13 09:31:09 +0500

    Add files via upload

---
+++ core/server.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#            ---------------------------------------------------
+#                              Evilip Framework                                                  
+#            ---------------------------------------------------
+#                Copyright (C) <2020-2021>  <EvilDevilWasi>
+#
+#        This program is free software: you can redistribute it and/or modify
+#        it under the terms of the GNU General Public License as published by
+#        the Free Software Foundation, either version 3 of the License, or
+#        any later version.
+#
+#        This program is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#        GNU General Public License for more details.
+#
+#        You should have received a copy of the GNU General Public License
+#        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+if [[ "$1" = "" ]] 
+then
+    echo -e "Usage: server.sh [start|stop]"
+else
+    if [[ "$1" = "start" ]]
+    then
+        {
+        adb start-server
+        } &> /dev/null
+    elif [[ "$1" = "stop" ]]
+    then
+        {
+        adb disconnect
+        adb kill-server
+        } &> /dev/null
+    else
+        echo -e "Usage: server.sh [start|stop]"
+    fi
+fi
